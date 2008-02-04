41d9782a479b3e7f95798d4420b8e224b4385971
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-02-04 12:56:25 +0000

    Trying to add multiline commands

--- trunk/content/collection.js
+++ trunk/content/collection.js
@@ -356,7 +356,7 @@ function getDir(mytype, id) {
     }
     else if (mytype == 'custom') {
         addnav('Command: '+id, 'custom', id)
-        cmd = id
+        cmd = id.replace(/\\n/g, "\n")
         id = ""
         var cb = function (data) {
             setTable(parse_db(data))
@@ -676,6 +676,14 @@ function files_keypress (event) {
         case 46: delete_item(); break;
         default: //alert(event.which);
             break;
+    }
+}
+function mpd_sent_keypress (e, event) {
+    if (event.altKey) {
+        if (event.keyCode == 13) { e.value += "\n" }
+    }
+    else {
+        (event.keyCode == 13) ? getDir('custom',e.value):null
     }
 }
 notify['db_update'] = function(v){
