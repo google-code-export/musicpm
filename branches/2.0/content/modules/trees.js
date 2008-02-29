56eebae093907cddfa12f155b245f03e496110fd
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-02-29 11:28:09 +0000

    Working on trees views, playlist xbl.

--- branches/2.0/content/modules/trees.js
+++ branches/2.0/content/modules/trees.js
@@ -21,21 +21,42 @@ EXPORTED_SYMBOLS = ["playlistView", "trees_EXPORTED_SYMBOLS"].concat(mpd_EXPORTE
 var trees_EXPORTED_SYMBOLS = copyArray(EXPORTED_SYMBOLS)
 
 
-function customView (db, tree) {
-	this.db = db
-	this.tree = tree
-    this.rowCount = db.length
+function customView () {
+	this.load = function (db){
+		var rc = db.length
+		var rs = new Array()
+		rs.length = rc
+		if (rc) {
+			var n = rc
+			do {
+				var i = rc - n
+				rs[i] = {type: 'pointer', index: i}
+			} while (--n)
+		}
+		this.db = db;
+		this.rs = rs
+		this.rowCount = rc
+	}
+	this.emptyRecord = {type: 'unknown', Title: 'Loading...', isOpen: false}
+	this.get = function (row) {
+		var cur = Nz(this.rs[row], this.emptyRecord)
+		if (cur.type == 'pointer') {
+			return Nz(this.db[cur.index], this.emptyRecord)
+		}
+		return cur
+	}
     this.getCellText = function(R, C){
-        if (!Nz(db[R])) return "";
+		var item = this.get(R)
+        if (!Nz(item)) return "";
         switch (C.id) {
             case "Time":
-                return hmsFromSec(db[R][C.id]);
+                return (item.Time) ? hmsFromSec(item.Time) : "";
                 break;
             case "Pos":
-                return (parseInt(R) + 1) + ".";
+                return (item.Pos) ? (parseInt(item.Pos) + 1) + "." : "";
                 break;
             default:
-                return db[R][C.id];
+                return item[C.id];
                 break;
         }
     }
@@ -62,8 +83,9 @@ function customView (db, tree) {
     }
     this.getRowProperties = function(row, props){
         try {
+			var item = this.get(R)
             var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-            props.AppendElement(aserv.getAtom(db[row].type))
+            props.AppendElement(aserv.getAtom(item.type))
             aserv = null
         }
         catch (e) {
@@ -71,10 +93,11 @@ function customView (db, tree) {
     }
     this.getCellProperties = function(row, col, props){
         try {
-            var t = db[row].type
+			var item = this.get(row)
+            var t = item.type
             var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
             props.AppendElement(aserv.getAtom(col.id + "_" + t))
-            if (t == 'file' && db[row].Name == mpd.song) {
+            if (t == 'file' && item.file == mpd.file) {
                 if (col.id == 'Title' && mpd.state != 'stop') {
                     props.AppendElement(aserv.getAtom(mpd.state + "_currentsong"))
                 }
@@ -87,6 +110,7 @@ function customView (db, tree) {
             aserv = null
         }
         catch (e) {
+			debug(e)
         }
     }
     this.getParentIndex = function(idx){
@@ -101,128 +125,103 @@ function customView (db, tree) {
     }
 }
 
-function playlistView(db, tree){
-    this.rowCount = db.length
-	this.getCellText = function(R, C){
-		if (!Nz(db[R])) 
-			return "";
-		if (db[R].type == 'file') {
-			switch (C.id) {
-				case "Time":
-					return hmsFromSec(db[R][C.id]);
-					break;
-				case "Pos":
-					return (parseInt(R) + 1) + ".";
-					break;
-				default:
-					return db[R][C.id];
-					break;
-			}
-		}
-		else 
-			if (C.id == 'Title') 
-				return db[R].type + ": " + db[R].Title
-	}
+function playlistView(){
 	this.isContainer = function(row){
-		return (db[row].type == 'file')
+		try {
+			return (this.rs[row].type == 'pointer')
+		} 
+		catch (e) {
+			return false
+		}
 	}
 	this.getParentIndex = function(idx){
-		return (db[idx].type == 'file') ? 0 : -1
+		try {
+			return (this.rs[idx].type == 'pointer') ? -1 : 0
+		} 
+		catch (e) {
+			return -1
+		}
 	}
 	this.getLevel = function(row){
-		return (db[row].type == 'file') ? 0 : 1
+		try {
+			return (this.rs[row].type == 'pointer') ? 0 : 1
+		} 
+		catch (e) {
+			return 0
+		}
 	}
 	this.hasNextSibling = function(row, afterIndex){
-		return (db[afterIndex].type == db[row].type)
+		var item = this.get(row)
+		var after = this.get(afterIndex)
+		if (item.type == 'pointer') {
+			return (after.type == 'pointer')
+		}
+		return !(after.type == 'pointer')
 	}
 	this.isContainerEmpty = function(row){
 		return false
 	}
 	this.isContainerOpen = function(row){
-		return (Nz(db[row + 1])) ? (db[row + 1].type != 'file') : false
+		try {
+			return this.rs[row].isOpen
+		} 
+		catch (e) {
+			return false
+		}
 	}
 	this.toggleOpenState = function(row){
 		try {
-			var is_open = (db[row + 1].type != 'file')
-		} 
-		catch (e) {
-			var is_open = false
-		}
-		if (is_open) {
-			db.splice(parseInt(row) + 1, 2)
-			playlistView.treebox.rowCountChanged(row + 1, -2)
-		}
-		else {
-			var art = {
-				type: 'Artist',
-				Pos: db[row].Pos,
-				Title: Nz(db[row].Artist, 'unknown')
+			if (this.rs[row].isOpen) {
+				this.rs[row].isOpen = false
+				this.rs.splice(parseInt(row)+1, 2)
+				this.treebox.rowCountChanged(row+1, -2)
 			}
-			var alb = {
-				type: 'Album',
-				Pos: db[row].Pos,
-				Title: Nz(db[row].Album, 'unknown')
+			else {
+				var item = this.get(row)
+				var art = {
+					index: item.Pos,
+					type: 'Artist',
+					Pos: null,
+					Title: "by "+Nz(item.Artist, 'unknown'),
+					Time: null
+				}
+				var alb = {
+					index: item.Pos,
+					type: 'Album',
+					Pos: null,
+					Title: "from "+Nz(item.Album, 'unknown'),
+					Time: null
+				}
+				this.rs[row].isOpen = true
+				this.rs.splice(row+1, 0, alb, art)
+				this.treebox.rowCountChanged(row+1, 2)
 			}
-			db.splice(row + 1, 0, alb, art)
-			playlistView.treebox.rowCountChanged(row + 1, 2)
-		}
-	}
-	this.getRowProperties = function(row, props){
-		try {
-			var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-			props.AppendElement(aserv.getAtom(db[row].type))
-			var P = (parseInt(db[row].Pos) % 2) ? 'odd' : 'even'
-			var R = (row % 2) ? 'odd' : 'even'
-			if (P != R) {
-				props.RemoveElement(aserv.getAtom(R))
-				props.AppendElement(aserv.getAtom(P))
-			}
-			aserv = null
 		} 
 		catch (e) {
 			debug(e)
 		}
 	}
-    this.setTree = function(treebox){
-        this.treebox = treebox;
-    }
-    this.isSeparator = function(row){
-        return false;
-    }
-    this.isSorted = function(){
-        return false;
-    }
-    this.cycleHeader = function(col, elem){
-        return null;
-    }
-    this.getImageSrc = function(row, col){
-        return null;
-    }
-    this.getCellProperties = function(row, col, props){
-        try {
-            var t = db[row].type
-            var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-            props.AppendElement(aserv.getAtom(col.id + "_" + t))
-            if (t == 'file' && db[row].Name == mpd.song) {
-                if (col.id == 'Title' && mpd.state != 'stop') {
-                    props.AppendElement(aserv.getAtom(mpd.state + "_currentsong"))
-                }
-                props.AppendElement(aserv.getAtom("currentsong"))
-            }
-            props.AppendElement(aserv.getAtom(col.id))
-            if (t != 'file' && t != 'playlist' && mpd.updating_db) {
-                props.AppendElement(aserv.getAtom(col.id + "_updating"))
-            }
-            aserv = null
-        }
-        catch (e) {
-        }
-    }
-    this.getColumnProperties = function(colid, col, props){
-    }
-    this.canDrop = function(index, orient){
-        return false
-    }
-    this.drop = function(row, orient){
+	this.getRowProperties = function(row, props){
+		try {
+			var item = this.rs[row]
+			if (item.type != 'unknown') {
+				var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
+				props.AppendElement(aserv.getAtom(item.type))
+				var P = (parseInt(item.index) % 2) ? 'odd' : 'even'
+				var R = (row % 2) ? 'odd' : 'even'
+				if (P != R) {
+					props.RemoveElement(aserv.getAtom(R))
+					props.AppendElement(aserv.getAtom(P))
+				}
+				aserv = null
+			}
+		} 
+		catch (e) {
+		}
+	}
+	this.expandCurrent = function (row) {
+		if (!this.isContainerOpen(row))this.toggleOpenState(row)
 	}
 }
+
+playlistView.prototype = new customView
