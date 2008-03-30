1a8c9fa461ec81b8be8ff03122ec4c26cbf6312a
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-03-30 15:42:49 +0000

    ..

---
+++ branches/2.0_xulrunner/components/mpd-autocomplete.js
@@ -0,0 +1,224 @@
+/**    Music Player Minion  Copyright 2008, Chris Seickel
+*
+*      This program is free software; you can redistribute it and/or modify
+*      it under the terms of the GNU General Public License as published by
+*      the Free Software Foundation; either version 2 of the License, or
+*      (at your option) any later version.
+*
+*      This program is distributed in the hope that it will be useful,
+*      but WITHOUT ANY WARRANTY; without even the implied warranty of
+*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*      GNU General Public License for more details.
+*
+*      You should have received a copy of the GNU General Public License
+*      along with this program; if not, write to the Free Software
+*      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*      MA 02110-1301, USA.
+*/
+
+//Components.utils.import("resource://minion/mpd.js");
+const Ci = Components.interfaces;
+
+const CLASS_ID = Components.ID("6224daa1-71a2-4d1a-ad90-01ca1c08e323");
+const CLASS_NAME = "Simple AutoComplete";
+const CONTRACT_ID = "@mozilla.org/autocomplete/search;1?name=simple-autocomplete";
+
+
+var dbfile = Components.classes["@mozilla.org/file/directory_service;1"]
+                     .getService(Components.interfaces.nsIProperties)
+						.get("Home", Components.interfaces.nsIFile);
+dbfile.append("mpd.sqlite");
+var mDBConn = Components.classes["@mozilla.org/storage/service;1"]
+                        .getService(Components.interfaces.mozIStorageService)
+							.openDatabase(dbfile);
+
+// Implements nsIAutoCompleteResult
+function SimpleAutoCompleteResult(searchString, searchResult,
+                                  defaultIndex, errorDescription,
+                                  results, comments) {
+  this._searchString = searchString;
+  this._searchResult = searchResult;
+  this._defaultIndex = defaultIndex;
+  this._errorDescription = errorDescription;
+  this._results = results;
+  this._comments = comments;
+}
+
+SimpleAutoCompleteResult.prototype = {
+  _searchString: "",
+  _searchResult: 0,
+  _defaultIndex: 0,
+  _errorDescription: "",
+  _results: [],
+  _comments: [],
+
+  /**
+   * The original search string
+   */
+  get searchString() {
+    return this._searchString;
+  },
+
+  /**
+   * The result code of this result object, either:
+   *         RESULT_IGNORED   (invalid searchString)
+   *         RESULT_FAILURE   (failure)
+   *         RESULT_NOMATCH   (no matches found)
+   *         RESULT_SUCCESS   (matches found)
+   */
+  get searchResult() {
+    return this._searchResult;
+  },
+
+  /**
+   * Index of the default item that should be entered if none is selected
+   */
+  get defaultIndex() {
+    return this._defaultIndex;
+  },
+
+  /**
+   * A string describing the cause of a search failure
+   */
+  get errorDescription() {
+    return this._errorDescription;
+  },
+
+  /**
+   * The number of matches
+   */
+  get matchCount() {
+    return this._results.length;
+  },
+
+  /**
+   * Get the value of the result at the given index
+   */
+  getValueAt: function(index) {
+    return this._results[index];
+  },
+
+  /**
+   * Get the comment of the result at the given index
+   */
+  getCommentAt: function(index) {
+    return this._comments[index];
+  },
+
+  /**
+   * Get the style hint for the result at the given index
+   */
+  getStyleAt: function(index) {
+    if (!this._comments[index])
+      return null;  // not a category label, so no special styling
+
+    if (index == 0)
+      return "suggestfirst";  // category label on first line of results
+
+    return "suggesthint";   // category label on any other line of results
+  },
+
+  /**
+   * Remove the value at the given index from the autocomplete results.
+   * If removeFromDb is set to true, the value should be removed from
+   * persistent storage as well.
+   */
+  removeValueAt: function(index, removeFromDb) {
+    this._results.splice(index, 1);
+    this._comments.splice(index, 1);
+  },
+
+  QueryInterface: function(aIID) {
+    if (!aIID.equals(Ci.nsIAutoCompleteResult) && !aIID.equals(Ci.nsISupports))
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+};
+
+
+// Implements nsIAutoCompleteSearch
+function SimpleAutoCompleteSearch() {
+}
+
+SimpleAutoCompleteSearch.prototype = {
+  /*
+   * Search for a given string and notify a listener (either synchronously
+   * or asynchronously) of the result
+   *
+   * @param searchString - The string to search for
+   * @param searchParam - An extra parameter
+   * @param previousResult - A previous result to use for faster searchinig
+   * @param listener - A listener to notify when the search is complete
+   */
+  startSearch: function(searchString, searchParam, result, listener) {
+	var i = searchString.indexOf('://')
+	if (i > -1) {
+		var type = searchString.slice(0,i)
+	}
+	else {
+		var type = 'home'
+	}
+	var results = []
+	var comments = []
+	var q = mDBConn.createStatement("select URI, title from " + type +
+		" where URI like '" + searchString + "%'")
+	while (q.executeStep()) {
+		results.push(q.getUTF8String(0))
+		comments.push(q.getUTF8String(1))
+	}
+	var newResult = new SimpleAutoCompleteResult(searchString, Ci.nsIAutoCompleteResult.RESULT_SUCCESS, 0, "", results, comments);
+	listener.onSearchResult(this, newResult);
+  },
+
+  /*
+   * Stop an asynchronous search that is in progress
+   */
+  stopSearch: function() {
+  },
+    
+  QueryInterface: function(aIID) {
+    if (!aIID.equals(Ci.nsIAutoCompleteSearch) && !aIID.equals(Ci.nsISupports))
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+};
+
+// Factory
+var SimpleAutoCompleteSearchFactory = {
+  singleton: null,
+  createInstance: function (aOuter, aIID) {
+    if (aOuter != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    if (this.singleton == null)
+      this.singleton = new SimpleAutoCompleteSearch();
+    return this.singleton.QueryInterface(aIID);
+  }
+};
+
+// Module
+var SimpleAutoCompleteSearchModule = {
+  registerSelf: function(aCompMgr, aFileSpec, aLocation, aType) {
+    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    aCompMgr.registerFactoryLocation(CLASS_ID, CLASS_NAME, CONTRACT_ID, aFileSpec, aLocation, aType);
+  },
+
+  unregisterSelf: function(aCompMgr, aLocation, aType) {
+    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    aCompMgr.unregisterFactoryLocation(CLASS_ID, aLocation);        
+  },
+  
+  getClassObject: function(aCompMgr, aCID, aIID) {
+    if (!aIID.equals(Components.interfaces.nsIFactory))
+      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+    if (aCID.equals(CLASS_ID))
+      return SimpleAutoCompleteSearchFactory;
+
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+  },
+
+  canUnload: function(aCompMgr) { return true; }
+};
+
+// Module initialization
+function NSGetModule(aCompMgr, aFileSpec) { return SimpleAutoCompleteSearchModule; }
