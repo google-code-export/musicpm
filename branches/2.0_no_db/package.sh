e6bb8da9caa7f77a79009eb84280465871e18a10
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-11-05 01:50:26 +0000

    fix package script

--- branches/2.0_no_db/package.sh
+++ branches/2.0_no_db/package.sh
@@ -4,6 +4,6 @@ cp -R * mpm_$1
 cd mpm_$1
 find . -name ".svn" -exec rm -rf {} \;
 find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed -i 's/__mpm_version__/$1/g'
+zip -r mpm_$1.xpi *
 cd ..
-zip -r mpm_$1.xpi mpm_$1
 rm -rf mpm_$1
