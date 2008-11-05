469949d9198035c6efa36cc2d12fda69a721cb72
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-11-05 02:59:59 +0000

    more package script work

--- branches/2.0_no_db/package.sh
+++ branches/2.0_no_db/package.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-rm mpm_$1.xpi
-cp -R * release/
-cd release
+rm -rf ../mpm_release
+mkdir ../mpm_release
+cp -R * ../mpm_release
+cd ../mpm_release
 find . -type d -name ".svn*" -exec rm -rf {} \;
 find . -type f -name ".svn*" -exec rm -rf {} \;
-find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed -i 's/__mpm_version__/$1/g'
+find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed -i "s/__mpm_version__/$1/g"
 zip -r mpm_$1.xpi *
-cd ..
-rm -rf release
