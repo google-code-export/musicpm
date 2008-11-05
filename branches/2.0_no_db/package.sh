144316a4b14e461501fa91e91fc958c8466d7660
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-11-05 02:10:26 +0000

    Really fixed package.sh now.

--- branches/2.0_no_db/package.sh
+++ branches/2.0_no_db/package.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mkdir mpm_$1
-cp -R * mpm_$1
-cd mpm_$1
-find . -name ".svn" -exec rm -rf {} \;
+rm mpm_$1.xpi
+cp -R * release/
+cd release
+find . -type d -name ".svn*" -exec rm -rf {} \;
+find . -type f -name ".svn*" -exec rm -rf {} \;
 find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed -i 's/__mpm_version__/$1/g'
 zip -r mpm_$1.xpi *
 cd ..
-rm -rf mpm_$1
+rm -rf release
