653267f4585b50441cff2342edfe368ce9b1a940
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-11-05 01:44:41 +0000

    Added package.sh script to svn

--- branches/2.0_no_db/package.sh
+++ branches/2.0_no_db/package.sh
@@ -3,7 +3,7 @@ mkdir mpm_$1
 cp -R * mpm_$1
 cd mpm_$1
 find . -name ".svn" -exec rm -rf {} \;
-sed 's/mpm_version/$1/g' find .
+find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed 's/__mpm_version__/$1/g'
 cd ..
 zip -r mpm_$1.xpi mpm_$1
-
+rm -rf mpm_$1
