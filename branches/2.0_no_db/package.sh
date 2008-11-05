ca0ea4f52cea9273f3f4feb45275af4a8b754291
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2008-11-05 01:48:24 +0000

    replaced version numbers with __mpm_version__ so they can be autoreplaced with sed.

--- branches/2.0_no_db/package.sh
+++ branches/2.0_no_db/package.sh
@@ -3,7 +3,7 @@ mkdir mpm_$1
 cp -R * mpm_$1
 cd mpm_$1
 find . -name ".svn" -exec rm -rf {} \;
-find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed 's/__mpm_version__/$1/g'
+find . \( -iname "*.js" -or -iname "*.rdf" -or -iname "*.x?l" -or -iname "*.html" \) -type f | xargs sed -i 's/__mpm_version__/$1/g'
 cd ..
 zip -r mpm_$1.xpi mpm_$1
 rm -rf mpm_$1
