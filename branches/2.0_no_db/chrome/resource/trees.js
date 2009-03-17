6cc82704d79316f1a165156694281df3d67ae952
cseickel <cseickel@30035ed8-bb42-0410-b567-69195701006a>
2009-03-17 00:05:21 +0000

    Begining to refactor...

--- branches/2.0_no_db/chrome/resource/trees.js
+++ branches/2.0_no_db/chrome/resource/trees.js
@@ -22,19 +22,19 @@ EXPORTED_SYMBOLS = ["arrayView", "playlistView", "folderView"]
 
 
 function customTreeView () {
-	this.getRowCount = function() {return this.rs.length},
-	this.rs = []
-	this.db = []
-	this.treeBox = null
-	this.rowCount = 0
+    this.getRowCount = function() {return this.rs.length},
+    this.rs = []
+    this.db = []
+    this.treeBox = null
+    this.rowCount = 0
         this.get = function (row) {return this.rs[row]}
-	this.canDrop = function (index, orientation ) {return false}
-	this.cycleCell = function (row, col ) {}
-	this.cycleHeader = function (col ) {}
-	this.drop = function (row, orientation ) {}
-	this.getCellProperties = function (row, col, props ) {
+    this.canDrop = function (index, orientation ) {return false}
+    this.cycleCell = function (row, col ) {}
+    this.cycleHeader = function (col ) {}
+    this.drop = function (row, orientation ) {}
+    this.getCellProperties = function (row, col, props ) {
         try {
-			var item = Nz(this.get(row))
+            var item = Nz(this.get(row))
             if (item) {
                 var t = item.type
                 var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
@@ -56,12 +56,12 @@ function customTreeView () {
             }
         }
         catch (e) {
-			debug(e)
+            debug(e)
             debug(item)
         }
     }
     this.getCellText = function(R, C){
-		var item = this.get(R)
+        var item = this.get(R)
         if (!Nz(item)) return "";
         switch (C.id) {
             case "Pos":
@@ -78,40 +78,40 @@ function customTreeView () {
                 break;
         }
     }
-	this.getCellValue = function (row, col ) {return this.rs[row]}
-	this.getColumnProperties = function (col, properties ) {}
-	this.getImageSrc  = function (row, col ) {return ''}
-	this.getLevel = function  (index ) {return 0}
-	this.getParentIndex  = function (rowIndex ) {return -1}
-	this.getProgressMode  = function (row, col ) {return 3}
-	this.getRowProperties = function(index, properties){
-		try {
-			var item = this.get(index)
-			var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-			props.AppendElement(aserv.getAtom(item.type))
-			aserv = null
-		}
-		catch (e) {
-		}
-	}
-	this.hasNextSibling = function (rowIndex, afterIndex ) {return true}
-	this.isContainer  = function (index ) {return false}
-	this.isContainerEmpty  = function (index ) {return false}
-	this.isContainerOpen  = function (index ) {return false}
-	this.isEditable  = function (row, col ) {return false}
-	this.isSelectable  = function (row, col ) {return false}
-	this.isSeparator  = function (index ) {return false}
-	this.isSorted  = function ( ) {return false}
-	this.performAction  = function (action ) {}
-	this.performActionOnCell  = function (action, row, col ) {}
-	this.performActionOnRow  = function (action, row ) {}
-	this.selectionChanged  = function ( ) {}
-	this.setCellText  = function (row, col, value ) {}
-	this.setCellValue  = function (row, col, value ) {}
-	this.setTree  = function ( tree ) {
+    this.getCellValue = function (row, col ) {return this.rs[row]}
+    this.getColumnProperties = function (col, properties ) {}
+    this.getImageSrc  = function (row, col ) {return ''}
+    this.getLevel = function  (index ) {return 0}
+    this.getParentIndex  = function (rowIndex ) {return -1}
+    this.getProgressMode  = function (row, col ) {return 3}
+    this.getRowProperties = function(index, properties){
+        try {
+            var item = this.get(index)
+            var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
+            props.AppendElement(aserv.getAtom(item.type))
+            aserv = null
+        }
+        catch (e) {
+        }
+    }
+    this.hasNextSibling = function (rowIndex, afterIndex ) {return true}
+    this.isContainer  = function (index ) {return false}
+    this.isContainerEmpty  = function (index ) {return false}
+    this.isContainerOpen  = function (index ) {return false}
+    this.isEditable  = function (row, col ) {return false}
+    this.isSelectable  = function (row, col ) {return false}
+    this.isSeparator  = function (index ) {return false}
+    this.isSorted  = function ( ) {return false}
+    this.performAction  = function (action ) {}
+    this.performActionOnCell  = function (action, row, col ) {}
+    this.performActionOnRow  = function (action, row ) {}
+    this.selectionChanged  = function ( ) {}
+    this.setCellText  = function (row, col, value ) {}
+    this.setCellValue  = function (row, col, value ) {}
+    this.setTree  = function ( tree ) {
         this.treeBox = tree
     }
-	this.toggleOpenState  = function (index ) {}
+    this.toggleOpenState  = function (index ) {}
 }
 
 
@@ -122,221 +122,221 @@ function arrayView(dbArray) {
 arrayView.prototype = new customTreeView
 
 function playlistView(){
-	this.getPointer = function (idx) {
-		return {
-			type: 'pointer',
-			index: idx,
-			Title: 'Loading...',
-			pointer: "loading...",
-			isContainer: false,
-			isOpen: false
-		}
-	}
-	this.load = function (db, expanded){
-		if (!Nz(expanded)) {
-			var rc = db.length
-			var rs = new Array()
-			rs.length = rc
-			if (rc) {
-				var n = rc
-				do {
-					var i = rc - n
-					rs[i] = this.getPointer(i)
+    this.getPointer = function (idx) {
+        return {
+            type: 'pointer',
+            index: idx,
+            Title: 'Loading...',
+            pointer: "loading...",
+            isContainer: false,
+            isOpen: false
+        }
+    }
+    this.load = function (db, expanded){
+        if (!Nz(expanded)) {
+            var rc = db.length
+            var rs = new Array()
+            rs.length = rc
+            if (rc) {
+                var n = rc
+                do {
+                    var i = rc - n
+                    rs[i] = this.getPointer(i)
 
-				}
-				while (--n)
-			}
-		}
-		else {
-			var rc = db.length * 3
-			var rs = new Array()
-			rs.length = rc
-			if (rc) {
-				var n = rc
-				var idx = 0
-				do {
-					rs[rc - n] = this.getPointer(idx)
-					rs[rc - n].isOpen = true
-					--n
-					rs[rc - n] = {
-						index: idx,
-						type: 'Album',
-						Prefix: "from ",
-						isContainer: false
-					}
-					--n
-					rs[rc - n] = {
-						index: idx,
-						type: 'Artist',
-						Prefix: "by ",
-						isContainer: false
-					}
-					idx++
-				}
-				while (--n)
-			}
-		}
-		if (this.treeBox) {
-			var chg = rs.length-this.rs.length
-			var n = (chg < 0) ? 1 : this.rs.length
-			this.treeBox.rowCountChanged(n-1, chg)
-			this.treeBox.invalidate()
-			this.treeBox.scrollToRow(0)
-		}
-		this.db = db;
-		this.rs = rs;
-		this.rowCount = this.rs.length;
-	}
-	this.addItem = function (idx, expanded) {
-		var i = this.rs.length
-		var nw = this.getPointer(idx)
-		this.rs[i] = nw
-		if (expanded) {
-			this.rs[i].isOpen = true
-			this.rs[i+1] = {
-				index: idx,
-				type: 'Album',
-				Prefix: "from ",
-				isContainer: false
-			}
-			this.rs[i+2] = {
-				index: idx,
-				type: 'Artist',
-				Prefix: "by ",
-				isContainer: false
-			}
-			this.treeBox.rowCountChanged(i-1, 3)
-		}
-		else {
-			this.treeBox.rowCountChanged(i-1, 1)
-		}
-	}
-	this.removeItems = function (count) {
-		var n = this.rs.length
-		for (var i = 0; i < count; i++) {
-			--n
-			if (n > 0) {
-				if (!this.isContainer(n)) {
-					var n = this.getParentIndex(n)
-				}
-			}
-			else {
-				n = 0
-				i = count
-			}
-		}
-		this.treeBox.rowCountChanged(n-1, n-this.rs.length)
-		this.rs.length = n
-	}
-	this.get = function (row) {
-		var cur = Nz(this.rs[row])
-		if (!cur) {
-			//this.treeBox.rowCountChanged(row, -1)
-			return {type: 'unknown', pos: null, time: null, Title: 'ERROR!'}
-		}
-		if (cur.type == 'pointer') {
-			return Nz(this.db[cur.index], cur)
-		}
-		else {
-			var item = Nz(this.db[cur.index], cur)
-			return {
-				type: cur.type,
-				index: cur.index,
-				time: null,
-				pos: null,
-				Title: cur.Prefix+item[cur.type]
-			}
-		}
-	}
-	this.getPointer = function (idx) {
-		return {
-			type: 'pointer',
-			index: idx,
-			Title: 'Loading...',
-			pointer: "loading...",
-			isContainer: true,
-			isOpen: false
-		}
-	}
-	this.isContainerOpen = function(row){
-		return this.rs[row].isOpen
-	}
-	this.isContainer = function(row){
-		return (this.rs[row].isContainer)
-	}
-	this.getParentIndex = function(idx){
-		if (this.rs[idx].isContainer) return -1
-		do {
-			--idx
-		} while (!this.rs[idx].isContainer && idx > -1)
-		return idx
-	}
-	this.getLevel = function(row){
-		try {
-			return (this.rs[row].isContainer) ? 0 : 1
-		}
-		catch (e) {
-			debug(e)
-			return 0
-		}
-	}
-	this.hasNextSibling = function(row, afterIndex){
-		var item = this.rs[row]
-		var after = this.rs[afterIndex]
-		if (item.type == 'pointer') {
-			return (after.type == 'pointer')
-		}
-		return !(after.type == 'pointer')
-	}
-	this.toggleOpenState = function(row){
-		try {
-			if (this.rs[row].isOpen) {
-				this.rs[row].isOpen = false
-				this.rs.splice(parseInt(row) + 1, 2)
-				this.treeBox.rowCountChanged(row + 1, -2)
-			}
-			else {
-				var item = this.get(row)
-				var art = {
-					index: item.Pos,
-					type: 'Artist',
-					Prefix: "by ",
-					isContainer: false
-				}
-				var alb = {
-					index: item.Pos,
-					type: 'Album',
-					Prefix: "from ",
-					isContainer: false
-				}
-				this.rs[row].isOpen = true
-				this.rs.splice(row+1, 0, alb, art)
-				this.treeBox.rowCountChanged(row, 2)
-				//this.rowCount = this.rs.length
-			}
-		}
-		catch (e) {
-			debug(e)
-		}
-	}
-	this.getRowProperties = function(row, props){
-		try {
-			var item = this.rs[row]
-			if (item.type != 'unknown') {
-				var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-				props.AppendElement(aserv.getAtom(item.type))
-				var P = (parseInt(item.index) % 2) ? 'odd' : 'even'
-				var R = (row % 2) ? 'odd' : 'even'
-				if (P != R) {
-					props.RemoveElement(aserv.getAtom(R))
-					props.AppendElement(aserv.getAtom(P))
-				}
-				aserv = null
-			}
-		}
-		catch (e) {
-		}
-	}
+                }
+                while (--n)
+            }
+        }
+        else {
+            var rc = db.length * 3
+            var rs = new Array()
+            rs.length = rc
+            if (rc) {
+                var n = rc
+                var idx = 0
+                do {
+                    rs[rc - n] = this.getPointer(idx)
+                    rs[rc - n].isOpen = true
+                    --n
+                    rs[rc - n] = {
+                        index: idx,
+                        type: 'Album',
+                        Prefix: "from ",
+                        isContainer: false
+                    }
+                    --n
+                    rs[rc - n] = {
+                        index: idx,
+                        type: 'Artist',
+                        Prefix: "by ",
+                        isContainer: false
+                    }
+                    idx++
+                }
+                while (--n)
+            }
+        }
+        if (this.treeBox) {
+            var chg = rs.length-this.rs.length
+            var n = (chg < 0) ? 1 : this.rs.length
+            this.treeBox.rowCountChanged(n-1, chg)
+            this.treeBox.invalidate()
+            this.treeBox.scrollToRow(0)
+        }
+        this.db = db;
+        this.rs = rs;
+        this.rowCount = this.rs.length;
+    }
+    this.addItem = function (idx, expanded) {
+        var i = this.rs.length
+        var nw = this.getPointer(idx)
+        this.rs[i] = nw
+        if (expanded) {
+            this.rs[i].isOpen = true
+            this.rs[i+1] = {
+                index: idx,
+                type: 'Album',
+                Prefix: "from ",
+                isContainer: false
+            }
+            this.rs[i+2] = {
+                index: idx,
+                type: 'Artist',
+                Prefix: "by ",
+                isContainer: false
+            }
+            this.treeBox.rowCountChanged(i-1, 3)
+        }
+        else {
+            this.treeBox.rowCountChanged(i-1, 1)
+        }
+    }
+    this.removeItems = function (count) {
+        var n = this.rs.length
+        for (var i = 0; i < count; i++) {
+            --n
+            if (n > 0) {
+                if (!this.isContainer(n)) {
+                    var n = this.getParentIndex(n)
+                }
+            }
+            else {
+                n = 0
+                i = count
+            }
+        }
+        this.treeBox.rowCountChanged(n-1, n-this.rs.length)
+        this.rs.length = n
+    }
+    this.get = function (row) {
+        var cur = Nz(this.rs[row])
+        if (!cur) {
+            //this.treeBox.rowCountChanged(row, -1)
+            return {type: 'unknown', pos: null, time: null, Title: 'ERROR!'}
+        }
+        if (cur.type == 'pointer') {
+            return Nz(this.db[cur.index], cur)
+        }
+        else {
+            var item = Nz(this.db[cur.index], cur)
+            return {
+                type: cur.type,
+                index: cur.index,
+                time: null,
+                pos: null,
+                Title: cur.Prefix+item[cur.type]
+            }
+        }
+    }
+    this.getPointer = function (idx) {
+        return {
+            type: 'pointer',
+            index: idx,
+            Title: 'Loading...',
+            pointer: "loading...",
+            isContainer: true,
+            isOpen: false
+        }
+    }
+    this.isContainerOpen = function(row){
+        return this.rs[row].isOpen
+    }
+    this.isContainer = function(row){
+        return (this.rs[row].isContainer)
+    }
+    this.getParentIndex = function(idx){
+        if (this.rs[idx].isContainer) return -1
+        do {
+            --idx
+        } while (!this.rs[idx].isContainer && idx > -1)
+        return idx
+    }
+    this.getLevel = function(row){
+        try {
+            return (this.rs[row].isContainer) ? 0 : 1
+        }
+        catch (e) {
+            debug(e)
+            return 0
+        }
+    }
+    this.hasNextSibling = function(row, afterIndex){
+        var item = this.rs[row]
+        var after = this.rs[afterIndex]
+        if (item.type == 'pointer') {
+            return (after.type == 'pointer')
+        }
+        return !(after.type == 'pointer')
+    }
+    this.toggleOpenState = function(row){
+        try {
+            if (this.rs[row].isOpen) {
+                this.rs[row].isOpen = false
+                this.rs.splice(parseInt(row) + 1, 2)
+                this.treeBox.rowCountChanged(row + 1, -2)
+            }
+            else {
+                var item = this.get(row)
+                var art = {
+                    index: item.Pos,
+                    type: 'Artist',
+                    Prefix: "by ",
+                    isContainer: false
+                }
+                var alb = {
+                    index: item.Pos,
+                    type: 'Album',
+                    Prefix: "from ",
+                    isContainer: false
+                }
+                this.rs[row].isOpen = true
+                this.rs.splice(row+1, 0, alb, art)
+                this.treeBox.rowCountChanged(row, 2)
+                //this.rowCount = this.rs.length
+            }
+        }
+        catch (e) {
+            debug(e)
+        }
+    }
+    this.getRowProperties = function(row, props){
+        try {
+            var item = this.rs[row]
+            if (item.type != 'unknown') {
+                var aserv = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
+                props.AppendElement(aserv.getAtom(item.type))
+                var P = (parseInt(item.index) % 2) ? 'odd' : 'even'
+                var R = (row % 2) ? 'odd' : 'even'
+                if (P != R) {
+                    props.RemoveElement(aserv.getAtom(R))
+                    props.AppendElement(aserv.getAtom(P))
+                }
+                aserv = null
+            }
+        }
+        catch (e) {
+        }
+    }
 }
 playlistView.prototype = new customTreeView
 
@@ -383,9 +383,9 @@ function folderView(dbArray, rsArray) {
         toggleOpenState : function (index ) {
             var leaf = this.rs[index]
             if (this.isContainerOpen(index)) {
-		var i = 1
-		while (this.rs[index + i].level > leaf.level) {i++}
-		--i
+        var i = 1
+        while (this.rs[index + i].level > leaf.level) {i++}
+        --i
                 this.rs.splice(index+1, i)
                 this.treeBox.rowCountChanged(index + 1, -i)
             }
@@ -427,7 +427,7 @@ function folderView(dbArray, rsArray) {
         getCellText: function(R, C){
             return this.rs[R].Title
         },
-        getCellValue: function (row, col ) {debug(this.rs[row]);return this.rs[row]},
+        getCellValue: function (row, col ) {return this.rs[row]},
         getColumnProperties: function (col, properties ) {},
         getImageSrc : function (row, col ) {return ''},
         getProgressMode : function (row, col ) {return 3},
