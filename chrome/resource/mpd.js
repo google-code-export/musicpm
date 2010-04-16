afdc7fa88a7a99dbad59ea4b28091a08f6a65d32
aurelien.requiem <aurelien.requiem@30035ed8-bb42-0410-b567-69195701006a>
2010-04-16 12:27:57 +0000

    - reworked the cover prefetch - deal properly with firefox to minimize network traffic - fixed a nasty Firefox crash cause by nsIChannel   called synchronously from within a UI thread. - removed GetImageFromURL() that caused the crash

--- chrome/resource/mpd.js
+++ chrome/resource/mpd.js
@@ -767,42 +767,6 @@ mpd.getAllDirs = function(callBack) {
     })
 }
 
-// This function is for fetching an image file from a URL. 
-// Accepts a URL and returns the file. 
-// Returns empty if the file is not found (with an 404 error for instance). 
-// Tried with .jpg, .ico, .gif (even .html).
-// https://developer.mozilla.org/en/Code_snippets/Downloading_Files
-
-function GetImageFromURL(url) { 
-	try {
-		var ioserv = Components.classes["@mozilla.org/network/io-service;1"] 
-					.getService(Components.interfaces.nsIIOService); 
-		var channel = ioserv.newChannel(url, 0, null); 
-		var stream = channel.open(); 
-
-		if (channel instanceof Components.interfaces.nsIHttpChannel && channel.responseStatus != 200) { 
-			return ""; 
-		}
-
-		var bstream = Components.classes["@mozilla.org/binaryinputstream;1"] 
-			.createInstance(Components.interfaces.nsIBinaryInputStream); 
-		bstream.setInputStream(stream); 
-
-		var total = 0;
-		var size = 0; 
-		var file_data = ""; 
-		while(size = bstream.available()) { 
-			file_data += bstream.readBytes(size); 
-			total += size;
-		} 
-		return total;
-	} catch(e) { 
-		// If an exception is thrown, this is mostlikely because the image cannot be found.
-		// debug(e); 
-		return 0;
-	}
-}
-
 function SaveImageToURL(item,url) {
 	try {
 		debug('cbSaveImageToURL url=');
@@ -905,9 +869,12 @@ function getAmazonArt (item, img) {
 }
 
 mpd.getArt = function(item, img) {
-	var fallback = function () {
-		debug("fallback")
-		if (prefs.get("use_amazon_art", 1) >= 1 ) getAmazonArt(item, img)
+	var fallback = function (code, item, url) {
+		if ( code == 200 || code == 0 ) {
+			mpd.cachedArt[url] = url;
+			return;
+		}
+		if (prefs.get("use_amazon_art", 1) >= 1 ) getAmazonArt(item, item.objImg)
 		else {
 			img.src = "chrome://minion/content/images/album_blank.png"
 			img.removeAttribute("tooltiptext")
@@ -917,18 +884,15 @@ mpd.getArt = function(item, img) {
 	img.src = "chrome://minion/content/images/album_loading.png"
 	img.setAttribute("tooltiptext","...")
 
+	item.objImg = img;
 	if (prefs.get("use_custom_art", false)) {
 		var url = urlReplace(prefs.get("custom_art_url"), item)
 		debug("Attempting to fetch cover at " + url)
-		// do not img.onerror, it appears not to work...
-		if ( GetImageFromURL(url) < 1 ) {
-			fallback();
-		} else {
-			img.setAttribute("tooltiptext",url)
-			img.src = url;
-		}
+		if (typeof(mpd.cachedArt[url]) != 'string') prefetchImageFromURL(url, fallback, item);
+		img.setAttribute("tooltiptext",url);
+		img.src = url;
 	} else {
-		fallback()
+		fallback(404, item);
 	}
 }
 
